b59afb4493874ee27c155ccce9df6051b8a31add
Igor J. Santos <igor@pragma.team>
2023-03-16 09:33:02 +1100

    fix: github actions npm install

--- lib/commands/whereabouts.ts
+++ lib/commands/whereabouts.ts
@@ -1,6 +1,6 @@
 import type { App, Middleware, SlackCommandMiddlewareArgs } from "@slack/bolt";
 
-export default (app: App): Middleware<SlackCommandMiddlewareArgs> =>
+export default (_app: App): Middleware<SlackCommandMiddlewareArgs> =>
   async ({ ack, body, command, payload, respond }) => {
     // Log command request on CloudWatch
     console.log(JSON.stringify({ body, command, payload }));
@@ -9,9 +9,10 @@ export default (app: App): Middleware<SlackCommandMiddlewareArgs> =>
     await ack();
 
     // Get all users presence
-    const usersPresence = await app.client.users.getPresence();
-    const usersPresenceDataJson = JSON.parse(usersPresence as any);
+    // const usersPresence = await app.client.users.getPresence();
+    // const usersPresenceDataJson = JSON.parse(usersPresence as any);
 
     // Respond to the command request
-    await respond(usersPresenceDataJson);
+    // await respond(usersPresenceDataJson);
+    await respond(JSON.stringify({ body, command, payload }));
   };
