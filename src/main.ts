83c3ceda4110899dc3a1cf4697085eabdf25c6cf
Igor J. Santos <igor@pragma.team>
2023-03-20 09:40:53 +1100

    chore: add utility functions and increase lambda timeout

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 /// Slash Commands Imports
+import local from "@/commands/local";
 import whereabouts from "@/commands/whereabouts";
 
 import app, { AwsHandler, awsLambdaReceiver, errorHandler } from "@/app";
-
-/// AWS Lambda Handler
-export const handler: AwsHandler = async (event, context, callback) => {
-  const handler = await awsLambdaReceiver.start();
-  return handler(event, context, callback);
-};
 
 /// Error Handler
 app.error(errorHandler);
 
 /// Commands
 app.command("/whereabouts", whereabouts(app));
-app.command("/local", whereabouts(app));
+app.command("/local", local(app));
+
+/// AWS Lambda Handler
+export const handler: AwsHandler = async (event, context, callback) => {
+  const lambdaHandler = await awsLambdaReceiver.start();
+  return lambdaHandler(event, context, callback);
+};
