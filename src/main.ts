f143caddd275b5683c0070e473be40a4dcd07377
Igor J. Santos <igor@pragma.team>
2023-03-27 18:02:48 +1100

    chore: remove warmup implementation

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,7 @@
 import whereabouts from "@/commands/whereabouts";
 
 /// App Imports
-import app, {
-  AwsHandler,
-  awsLambdaReceiver,
-  AwsResponse,
-  errorHandler,
-} from "@/app";
+import app, { AwsHandler, awsLambdaReceiver, errorHandler } from "@/app";
 
 /// Error Handler
 app.error(errorHandler);
@@ -18,10 +13,11 @@ app.command("/whereabouts", whereabouts(app));
 /// AWS Lambda Handler
 export const handler: AwsHandler = async (event, context, callback) => {
   /** Immediate response for WarmUp plugin */
-  if (context.custom.source === "serverless-plugin-warmup") {
-    console.log("WarmUp - Lambda is warm!");
-    return "Lambda is warm!" as unknown as AwsResponse;
-  }
+  // TODO: Fix this
+  // if (context.custom.source === "serverless-plugin-warmup") {
+  //   console.log("WarmUp - Lambda is warm!");
+  //   return "Lambda is warm!" as unknown as AwsResponse;
+  // }
 
   const lambdaHandler = await awsLambdaReceiver.start();
   return lambdaHandler(event, context, callback);
