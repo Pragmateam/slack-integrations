17a1dcaee1f0ea5d0269d4f49838983b411550e5
Igor J. Santos <igor@pragma.team>
2023-03-28 11:31:17 +1100

    feat: modify user list filter on whereabouts command

--- src/commands/whereabouts.ts
+++ src/commands/whereabouts.ts
@@ -8,13 +8,12 @@ const PRAGMATEAM_EMAIL_DOMAIN = "@pragma.team";
 // const PRAGMATEAM_TEAM_ID = "unknown";
 
 const predicates = {
-  activeUsers: (m: Member) => !m.deleted,
-
   // TODO: shall we filter by team id?
   pragmaUsers: (m: Member) =>
     m.profile?.email?.includes(PRAGMATEAM_EMAIL_DOMAIN),
 
-  realUsers: (m: Member) => !m.is_app_user && !m.is_bot && !m.is_workflow_bot,
+  realActiveUsers: (m: Member) =>
+    !m.deleted && !m.is_app_user && !m.is_bot && !m.is_workflow_bot,
 };
 
 const userPresenceMapper = (client: WebClient) => async (member: Member) => {
@@ -46,36 +45,45 @@ const userPresenceMapper = (client: WebClient) => async (member: Member) => {
 
 export default (): Middleware<SlackCommandMiddlewareArgs> => {
   return async ({ ack, body, client, command, logger, payload, respond }) => {
+    // Acknowledge command request
+    await ack({
+      response_type: "ephemeral",
+      text: "*Working on it...* :hourglass_flowing_sand: ",
+      replace_original: true,
+      delete_original: true,
+      as_user: true,
+    });
+
+    // await respond({
+    //   response_type: "ephemeral",
+    //   text: "*Working on it...* :hourglass_flowing_sand: ",
+    //   as_user: true,
+    // });
+
     // Log command request on CloudWatch
     logger.debug("BODY", JSON.stringify(body, null, 2));
     logger.debug("COMMAND", JSON.stringify(command, null, 2));
     logger.debug("PAYLOAD", JSON.stringify(payload, null, 2));
     logger.debug("PAYLOAD::TEXT", payload.text);
 
-    // Acknowledge command request
-    await respond({
-      response_type: "ephemeral",
-      text: "*Working on it...* :hourglass_flowing_sand: ",
-      as_user: true,
-    });
-
     // Get all users
     const users = await client.users.list();
 
     // When something goes wrong, respond with a generic error message
     if (!users.ok) {
-      await ack({
+      await respond({
         response_type: "ephemeral",
         text: "Something went wrong! Please try again later.",
+        replace_original: true,
+        delete_original: true,
         as_user: true,
       });
     }
 
     const activeUsers = array.filterByPredicate(
       users.members!,
-      predicates.activeUsers,
-      predicates.pragmaUsers,
-      predicates.realUsers
+      predicates.realActiveUsers,
+      predicates.pragmaUsers
     );
 
     // Get all members presence (a.k.a. status -> active, away, etc.)
@@ -86,10 +94,15 @@ export default (): Middleware<SlackCommandMiddlewareArgs> => {
     // TODO: remove this line
     logger.debug("MEMBERS", members);
 
+    const response = members.map((m) => `${m.name} (${m.presence})`).join("\n");
+
     // Respond to the command request
-    await ack({
+    await respond({
       response_type: "ephemeral",
-      text: json.toFormattedString(members),
+      // text: json.toFormattedString(members),
+      text: response,
+      replace_original: true,
+      delete_original: true,
       as_user: true,
     });
   };
